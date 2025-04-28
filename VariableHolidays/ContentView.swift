c4bf386f2ba3d755b59f1deba27cab4acffe3863
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2025-04-28 22:50:22 +0200

    Initial Commit

---
+++ VariableHolidays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VariableHolidays
+//
+//  Created by Krzysztof Zaporowski on 28/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
